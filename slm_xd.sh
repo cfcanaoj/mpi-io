96020a6166504f3aa7f0b37d1fd589b67c099a36
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2024-12-07 23:38:33 +0900

    setup for XD2000

---
+++ slm_xd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=testio
+#SBATCH --partition=P-large-cfca
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=400G
+#SBATCH -o slmlog%J.out
+
+# Go to this job's working directory
+cd ${SLURM_SUBMIT_DIR}
+
+export OMP_NUM_THREADS=1
+
+date >& log.$SLURM_JOB_ID
+time srun ./iotest >> log.$SLURM_JOB_ID
+date >> log.$SLURM_JOB_ID
+
+
+
+
